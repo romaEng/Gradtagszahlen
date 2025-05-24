6ea749a705eca23adcd703c73f5d9c6afd5f1e4f
marvin <marvin.roy@fhnw.ch>
2025-05-24 14:21:09 +0200

    Added PowerShell script to set up project environment and run application for windows

---
+++ start_app_win.ps1
@@ -0,0 +1,30 @@
+# Get absolute path to the script directory
+$SCRIPT_DIR = $PSScriptRoot
+
+# Change to the project directory
+Set-Location -Path $SCRIPT_DIR
+
+# Refresh Git-Repository
+Write-Host "Git Pull..." -ForegroundColor Cyan
+git pull
+
+# Activate virtual environment
+Write-Host "Aktiviere virtuelle Umgebung..." -ForegroundColor Cyan
+if (Test-Path ".venv") {
+    .\.venv\Scripts\Activate.ps1
+    Write-Host "Virtuelle Umgebung erfolgreich aktiviert." -ForegroundColor Green
+} else {
+    Write-Host "Virtuelle Umgebung nicht gefunden. Erstelle neue Umgebung..." -ForegroundColor Yellow
+    python -m venv .venv
+    .\.venv\Scripts\activate
+    pip install -r requirements.txt
+    Write-Host "Virtuelle Umgebung erfolgreich erstellt und aktiviert." -ForegroundColor Green
+}
+
+# Start Python script
+Write-Host "Starte Anwendung..." -ForegroundColor Cyan
+python main.py
+
+# Keep terminal open after completion
+Write-Host "Vorgang abgeschlossen. Zum Beenden: [ENTER]" -ForegroundColor Green
+Read-Host

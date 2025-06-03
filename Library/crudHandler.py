e08dd734d02a90f8425d3b0a6c477751cfe10d7a
Marvin Roy <120552192+romaEng@users.noreply.github.com>
2025-06-03 16:05:09 +0200

    Commented other endpoints except for get.

--- Library/crudHandler.py
+++ Library/crudHandler.py
@@ -9,23 +9,23 @@ class CrudHandler():
         response = requests.get(f"{self.base_url}/{endpoint}", headers=self.headers)
         return response.json()
 
-    def post(self, endpoint, data):
-        response = requests.post(f"{self.base_url}/{endpoint}", json=data, headers=self.headers)
-        return response.json()
+    # def post(self, endpoint, data):
+    #     response = requests.post(f"{self.base_url}/{endpoint}", json=data, headers=self.headers)
+    #     return response.json()
 
-    def put(self, endpoint, data):
-        response = requests.put(f"{self.base_url}/{endpoint}", json=data, headers=self.headers)
-        return response.json()
+    # def put(self, endpoint, data):
+    #     response = requests.put(f"{self.base_url}/{endpoint}", json=data, headers=self.headers)
+    #     return response.json()
 
-    def delete(self, endpoint):
-        response = requests.delete(f"{self.base_url}/{endpoint}", headers=self.headers)
-        return response.status_code
+    # def delete(self, endpoint):
+    #     response = requests.delete(f"{self.base_url}/{endpoint}", headers=self.headers)
+    #     return response.status_code
     
-    def patch(self, endpoint, data):
-        response = requests.patch(f"{self.base_url}/{endpoint}", json=data, headers=self.headers)
-        return response.json()
+    # def patch(self, endpoint, data):
+    #     response = requests.patch(f"{self.base_url}/{endpoint}", json=data, headers=self.headers)
+    #     return response.json()
     
-    def head(self, endpoint):
-        response = requests.head(f"{self.base_url}/{endpoint}", headers=self.headers)
-        return response.headers
+    # def head(self, endpoint):
+    #     response = requests.head(f"{self.base_url}/{endpoint}", headers=self.headers)
+    #     return response.headers
     
